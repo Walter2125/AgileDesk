4d52327af21f4b6bd2f29a9aa8ab75068623c05d
Walter2125 <wavalladaresv@unah.hn>
2025-06-17 14:41:13 -0600

    correcciones

--- storage/framework/views/a8985518f65eedafffd2b88b1e5e7f09.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'button', 'class' => 'btn btn-secondary'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH C:\Users\Dell\Herd\AgileDesk\resources\views/components/secondary-button.blade.php ENDPATH**/ ?>
--- storage/framework/views/acd71f5cca3f612d717879eb717f1a01.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'btn btn-danger'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH C:\Users\Dell\Herd\AgileDesk\resources\views/components/danger-button.blade.php ENDPATH**/ ?>
